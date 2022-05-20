66cd6ede9fabe1e5a40afe5e4e1f0a521da19510
Gene <gjy80230@foxmail.com>
2022-05-20 15:23:29 +0800

    添加数据结构存储网址，优化添加网址功能

--- src/main.js
+++ src/main.js
@@ -1,18 +1,42 @@
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
+const hashMap = [
+    {logo: 'A', url: 'https://www.acfun.cn/'},
+    {logo: 'B', url: 'https://www.bilibili.com/'},
+]
+hashMap.forEach((node)=>{
+    const $li = $(`
+    <li>
+        <a href="${node.url}">
+            <div class="site">
+                <div class="logo">${node.logo}</div>
+                <div class="link">${node.url}</div>
+            </div>
+        </a> 
+    </li>
+    `).insertBefore($lastLi)
+})
+
 $('.addButton').on('click', ()=>{
     let url = window.prompt("请输入要添加的网址：")
     if(url.indexOf('http') !== 0){
         url = 'https://' + url
     }
-    const $siteList = $('.siteList')
-    const $lastLi = $siteList.find('li.last')
-    const $li = $(`
-    <li>
-        <a href="${url}">
-            <div class="site">
-                <div class="logo">${url[0]}</div>
-                <div class="link">${url}</div>
-            </div>
-        </a> 
-    </li>
-    `).insertBefore($lastLi)
+    hashMap.push({
+        logo: url[0],
+        url: url
+    })
+    $siteList.find('li:not(.last)').remove()
+    hashMap.forEach((node)=>{
+        const $li = $(`
+        <li>
+            <a href="${node.url}">
+                <div class="site">
+                    <div class="logo">${node.logo}</div>
+                    <div class="link">${node.url}</div>
+                </div>
+            </a> 
+        </li>
+        `).insertBefore($lastLi)
+    })
 })
